5f300c617eab79de0b7a491d558248eeab075a65
lijnrong <306372068@qq.com>
2019-10-11 18:11:02 +0800

    update: eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,13 @@
 module.exports = {
     parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    parserOptions: {
+        //指定ESLint可以解析JSX语法
+        ecmaVersion: 2019,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
     extends: [
         'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
         'plugin:react/recommended',
@@ -8,6 +16,7 @@ module.exports = {
         'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
         'prettier/react',
     ],
+
     settings: {
         react: {
             version: 'detect',
@@ -28,13 +37,14 @@ module.exports = {
     rules: {
         'prettier/prettier': 'error',
         '@typescript-eslint/indent': ['error', 4, { VariableDeclarator: 4, SwitchCase: 1 }],
-        '@typescript-eslint/no-unused-vars': 0,
+        '@typescript-eslint/no-unused-vars': 'error',
         '@typescript-eslint/interface-name-prefix': 0,
         '@typescript-eslint/explicit-member-accessibility': 0,
         '@typescript-eslint/no-triple-slash-reference': 0,
         '@typescript-eslint/triple-slash-reference': ['error', { path: 'always', types: 'never', lib: 'never' }],
         'no-console': ['warn', { allow: ['warn', 'error'] }],
         eqeqeq: ['warn', 'always'],
+        'typescript/class-name-casing': 2,
         // React相关校验规则
         'react/jsx-indent': [2, 4],
         'react/jsx-no-undef': [2, { allowGlobals: true }],
